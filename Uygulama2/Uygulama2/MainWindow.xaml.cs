4776b025209ab6324e76537971544150d246c358
Emir Öztürk <emirozturk3@gmail.com>
2021-10-15 11:07:34 +0300

    Uygulama 2

---
+++ Uygulama2/Uygulama2/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows;
+
+namespace Uygulama2
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            BtnSifreleAc.IsEnabled = RbSifrele.IsChecked.Value || RbAc.IsChecked.Value;
+        }
+
+        private void BtnSifreleAc_Click(object sender, RoutedEventArgs e)
+        {
+            if (RbSifrele.IsChecked.Value)
+                TbOutput.Text = Sifreleyici.Sifrele(TbInput.Text);
+            else if (RbAc.IsChecked.Value)
+                TbOutput.Text = Sifreleyici.Ac(TbInput.Text);
+            TbInput.Clear();
+            RbSifrele.IsChecked = false;
+            RbAc.IsChecked = false;
+        }
+    }
+}
---
+++ Uygulama2/Uygulama2/Sifreleyici.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uygulama2
+{
+    class Sifreleyici
+    {
+        public static string Sifrele(string metin)
+        {
+            //return new string(metin.Select(x => Convert.ToChar(x + 3)).ToArray());
+            string output = "";
+            //Stringin sırasıyla her elemanını elde edebilmem lazım
+            //Daha sonra bir döngü kurarak her elemanı gezmem lazım
+            foreach (var karakter in metin)
+            {
+                //Her karakter için 3 harf ileriye götürüp yeni karakteri outputa eklemem gerek
+                output += Convert.ToChar((karakter + 3) % 256);
+            }
+            return output;
+
+        }
+        public static string Ac(string metin)
+        {
+            //return new string(metin.Select(x => Convert.ToChar(x - 3)).ToArray());
+            string output = "";
+            //Stringin sırasıyla her elemanını elde edebilmem lazım
+            //Daha sonra bir döngü kurarak her elemanı gezmem lazım
+            foreach (var karakter in metin)
+            {
+                //Her karakter için 3 harf geriye götürüp yeni karakteri outputa eklemem gerek
+                output += Convert.ToChar(karakter - 3);
+            }
+            return output;
+        }
+    }
+}
