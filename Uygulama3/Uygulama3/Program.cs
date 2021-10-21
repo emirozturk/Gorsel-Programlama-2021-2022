4bc4b4f0f331f26fdfd5d2e55af2b06e7ba9683b
Emir Öztürk <emirozturk3@gmail.com>
2021-10-21 14:13:01 +0300

    Uygulama değişiklikleri

---
+++ Uygulama3/Uygulama3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Uygulama3
+{
+    class Sinif
+    {
+        public void Yazdir(int deger)
+        {
+            Console.WriteLine(deger);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int degisken = 235;
+            var x = new Sinif();
+            x.Yazdir(degisken);
+        }
+    }
+}
