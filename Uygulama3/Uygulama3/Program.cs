d9ad06358f226f11a0660f49b0d9f327707b299d
Emir Öztürk <emirozturk3@gmail.com>
2021-10-21 14:16:55 +0300

    Generic olmayan hali

--- Uygulama3/Uygulama3/Program.cs
+++ Uygulama3/Uygulama3/Program.cs
@@ -8,14 +8,22 @@ namespace Uygulama3
         {
             Console.WriteLine(deger);
         }
+        public void Yazdir(string metin)
+        {
+            Console.WriteLine(metin);
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
             int degisken = 235;
+            string degisken2 = "Yazdirilacak bir string";
+            float degisken3 = 3.235f;
             var x = new Sinif();
             x.Yazdir(degisken);
+            x.Yazdir(degisken2);
+            x.Yazdir(degisken3);
         }
     }
 }
