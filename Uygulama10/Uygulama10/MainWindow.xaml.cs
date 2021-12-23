d5a9bfffb927b9d4c14e65c20976319876594b81
Emir Öztürk <emirozturk3@gmail.com>
2021-12-23 15:59:01 +0300

    10 ve 11. Uygulama

---
+++ Uygulama10/Uygulama10/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Uygulama10
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int sayac = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnEkle_Click(object sender, RoutedEventArgs e)
+        {
+            Button tus = new Button()
+            {
+                Width = 50,
+                Height = 50,
+                Content = sayac.ToString(),
+                FontSize = 24,
+                Name = $"Btn{sayac}"
+            };
+            tus.Click += Tus_Click;
+            WpTuslar.Children.Add(tus);
+            sayac++;
+        }
+
+        private void Tus_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button)
+            {
+                var tus = sender as Button;
+                tus.Foreground = Brushes.White;
+                tus.Background = tus.Background == Brushes.Green ? Brushes.Red : Brushes.Green;
+            }
+        }
+
+        private void BtnSil_Click(object sender, RoutedEventArgs e)
+        {
+            UIElement bulunan = null;
+            foreach (var element in WpTuslar.Children)
+            {
+                if (element is Button && (element as Button).Name == TbAranan.Text)
+                    bulunan = element as UIElement;
+            }
+            WpTuslar.Children.Remove(bulunan);
+        }
+
+        private void BtnAra_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var element in WpTuslar.Children)
+            {
+                //LbBilesenler.Items.Add(element.ToString());
+                var aranan = TbAranan.Text;
+                if (element is Button)
+                {
+                    var tus = element as Button;
+                    if (tus.Name == aranan)
+                    {
+                        tus.Background = Brushes.Green;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Uygulama11/Uygulama11/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Uygulama11
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<string> eklenecekListesi = new List<string>() { "Toplantı", "Kontrol", "Yemek", "Mesai" };
+        public MainWindow()
+        {
+            InitializeComponent();
+            Listele();
+        }
+
+        private void Listele()
+        {
+            foreach(var eklenecek in eklenecekListesi)
+            {
+                var eleman = new ListeElemani(eklenecek,false,true);
+                LbEklenecek.Items.Add(eleman);
+            }
+        }
+
+        private void LbEklenecek_Click(object sender, RoutedEventArgs e)
+        {
+            var eleman = e.Source as ListeElemani;
+            eleman.TusDegistir();
+            LbEklenecek.Items.Remove(eleman);
+            LbEklenen.Items.Add(eleman);
+        }
+
+        private void LbEklenen_Click(object sender, RoutedEventArgs e)
+        {
+            var eleman = e.Source as ListeElemani;
+            eleman.TusDegistir();
+            LbEklenen.Items.Remove(eleman);
+            LbEklenecek.Items.Add(eleman);
+        }
+
+        private void Grid_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
